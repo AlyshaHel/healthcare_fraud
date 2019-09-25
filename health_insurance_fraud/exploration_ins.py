2531c41e2b368b54c486ae4d76dbca242ba39231
Alysha <alyshah@utexas.edu>
2019-09-24 22:06:48 -0500

    Added beneficiaries data to analysis, created plots for data ciz

--- health_insurance_fraud/exploration_ins.py
+++ health_insurance_fraud/exploration_ins.py
@@ -10,30 +10,72 @@ from sklearn.pipeline import Pipeline
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+pd.set_option('display.max_columns', 30)
+
 # https://www.kaggle.com/rohitrox/healthcare-provider-fraud-detection-analysis/downloads/healthcare-provider-fraud-detection-analysis.zip/1
 df_ipd = pd.read_csv('Train_Inpatientdata-1542865627584.csv')
+df_bene = pd.read_csv('Train_Beneficiarydata-1542865627584.csv')
 df_fraud = pd.read_csv('Train-1542865627584.csv')
 
-df = df_ipd.merge(df_fraud, on='Provider').head(1000)
-# ['BeneID', 'ClaimID', 'ClaimStartDt', 'ClaimEndDt', 'Provider',
-#        'InscClaimAmtReimbursed', 'AttendingPhysician', 'OperatingPhysician',
-#        'OtherPhysician', 'AdmissionDt', 'ClmAdmitDiagnosisCode',
-#        'DeductibleAmtPaid', 'DischargeDt', 'DiagnosisGroupCode',
-#        'ClmDiagnosisCode_1', 'ClmDiagnosisCode_2', 'ClmDiagnosisCode_3',
-#        'ClmDiagnosisCode_4', 'ClmDiagnosisCode_5', 'ClmDiagnosisCode_6',
-#        'ClmDiagnosisCode_7', 'ClmDiagnosisCode_8', 'ClmDiagnosisCode_9',
-#        'ClmDiagnosisCode_10', 'ClmProcedureCode_1', 'ClmProcedureCode_2',
-#        'ClmProcedureCode_3', 'ClmProcedureCode_4', 'ClmProcedureCode_5',
-#        'ClmProcedureCode_6', 'PotentialFraud']
+cols = ['ChronicCond_stroke', 'ChronicCond_rheumatoidarthritis',
+        'ChronicCond_Osteoporasis', 'ChronicCond_IschemicHeart',
+        'ChronicCond_ObstrPulmonary', 'ChronicCond_Depression',
+        'ChronicCond_Diabetes', 'ChronicCond_Heartfailure', 'ChronicCond_KidneyDisease',
+        'ChronicCond_Cancer', 'ChronicCond_Alzheimer']
 
-df.PotentialFraud.replace(to_replace={'Yes': '1', 'No': '0'}, inplace=True)
+df_bene[cols] = df_bene[cols].replace(2, 0)
+
+# Bar Chart of # of Each Diagnosis
+df_bar = df_bene[cols].sum(axis=0, numeric_only=True)
+y = df_bar.values
+x = []
+for i in cols:
+    j = i.split('_')[1]
+    x.append(j)
+plt.barh(x, y)
+
+df = df_ipd.merge(df_fraud, on='Provider')
+df_full = df.merge(df_bene, on='BeneID')
+
+# print(df_full.isna().sum() * 100 / df_full.shape[0])
+df_full_cols = df_full.columns
+df_full_values = (df_full.isna().sum() * 100 / df_full.shape[0]).values
+for i in range(len(df_full_values)):
+    if df_full_values[i] > 50.0:
+        label = df_full_cols[i]
+        df_full.drop(labels=label, axis=1)
+
+df_full.PotentialFraud.replace(to_replace={'Yes': '1', 'No': '0'}, inplace=True)
 df_fraud.PotentialFraud.replace(to_replace={'Yes': '1', 'No': '0'}, inplace=True)
 
+# Figures exploring data visualizations
+plt.figure()
 f, ax = plt.subplots(figsize=(7, 5))
 sns.countplot(x='PotentialFraud', data=df_fraud)
-_ = plt.title('# Fraud vs NonFraud')
+_ = plt.title('# Fraud vs NonFraud, Based on Provider')
 _ = plt.xlabel('Class (1==Fraud)')
-# plt.show()
+plt.show()
+
+plt.figure()
+g, ay = plt.subplots(figsize=(7, 5))
+sns.countplot(x='PotentialFraud', data=df_full)
+_ = plt.title('# Fraud vs NonFraud, Based on Claim')
+_ = plt.xlabel('Class (1==Fraud)')
+
+sns.set(rc={'figure.figsize': (12, 8)}, style='white')
+plt.figure()
+sns.countplot(x='State', hue='PotentialFraud', data=df_full,
+              order=df_full.State.value_counts().iloc[:10].index, orient="h")
+plt.figure()
+sns.countplot(x='ChronicCond_IschemicHeart', hue='PotentialFraud', data=df_full,
+              order=df_full.ChronicCond_IschemicHeart.value_counts().iloc[:10].index, orient="h")
+plt.figure()
+sns.countplot(x='Provider', data=df_full,
+              order=df_full.Provider.value_counts().iloc[:10].index, orient="h")
+
+df_full["ClmCount_Provider"] = \
+    df_full.groupby(['Provider'])['ClaimID'].transform('count')
+print(df_full["ClmCount_Provider"])
 
 df_subset = df[['Provider', 'InscClaimAmtReimbursed', 'ClmAdmitDiagnosisCode', 'DeductibleAmtPaid', 'PotentialFraud']]
 df_subset = df_subset[~df_subset['ClmAdmitDiagnosisCode'].astype(str).str.contains("[a-zA-Z]").fillna(False)]
